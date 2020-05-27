7882dd8d221e795b3e7e0ee68edc25b8d966502b
Roman T <5229211@gmail.com>
2020-05-27 18:11:53 +0300

    logging

--- bot.py
+++ bot.py
@@ -3,14 +3,14 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
 
-logging.basedConfig('format = %(asctime)s - %(levelname)s - %(message)s',
-level = login.INFO,
-filename = 'bot.log'
-)
+logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(message)s',
+                    level = logging.INFO,
+                    filename = 'bot.log'
+                    )
 
 def start_bot(bot, update):
     mytext = 'Привет, {}! Я простой бот и пока понимаю только комманду {}'.format(update.message.chat.first_name, '/start')
-
+    logging.info('Пользователь {} нажал /start'.format(update.message.chat.username))
     update.message.reply_text(mytext)
 
 def chat(bot, update):
@@ -21,7 +21,7 @@ def chat(bot, update):
 def main():
     updtr = Updater(settings.TELEGRAM_API_KEY)
     updtr.dispatcher.add_handler(CommandHandler('start', start_bot))
-    pdtr.dispatcher.add_handler(MessageHandler('Filters.txt', chat))
+    updtr.dispatcher.add_handler(MessageHandler('Filters.txt', chat))
     updtr.start_polling()
     updtr.idle()
 
